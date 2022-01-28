89774eb4aa031935a08f0826a8a6359b1034969d
Christopher Delos Reyes <delosreyeschris77@gmail.com>
2022-01-28 21:36:31 +0800

    setup swagger file

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
 const app = express();
 const port = process.env.PORT || 3000;
 const URL = 'mongodb://localhost/HumanApiDB';
+const swaggerJsDocs = YAML.load('./swagger.yml');
+const humanRouter = require('./routes/human');
 
 mongoose.connect(URL, { useNewUrlParser: true });
 
 const con = mongoose.connection;
+
+
 con.on('open', () => console.log('connected in mongodb.'));
 con.on('error', () => console.log('error in mongodb.'));
 app.use(express.json());
 
-const humanRouter = require('./routes/human');
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 app.use('/api', humanRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
